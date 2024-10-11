61e3a40ac333acc56d4dbdc68f042e08967796e7
Karol Koś <kosiyyu@gmail.com>
2024-10-11 21:43:26 +0200

    Add new docker compose

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,12 @@
 using System.Text;
 using HotDeskWebApp.Database;
+using HotDeskWebApp.Models;
 using HotDeskWebApp.Repositories;
 using HotDeskWebApp.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Polly;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -49,6 +51,54 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 var app = builder.Build();
+
+//
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var logger = services.GetRequiredService<ILogger<Program>>();
+
+    var retry = Policy
+        .Handle<Exception>()
+        .WaitAndRetry(
+            retryCount: 5,
+            sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+            onRetry: (exception, timeSpan, retryCount, context) =>
+            {
+                logger.LogWarning(exception, "Error connecting to the database. Retry attempt {RetryCount}", retryCount);
+            }
+        );
+
+    retry.Execute(() =>
+    {
+        try
+        {
+            var context = services.GetRequiredService<DatabaseContext>();
+            context.Database.Migrate();
+            
+            if (!context.Users.Any(u => u.IsAdmin))
+            {
+                var userService = services.GetRequiredService<IUserService>();
+                var adminUser = new UserDTO
+                {
+                    Username = "admin",
+                    Email = "admin@example.com",
+                    Password = "12345678"
+                };
+                userService.AddAsync(adminUser, true).GetAwaiter().GetResult();
+                context.SaveChanges();
+
+                logger.LogInformation("Admin user created successfully.");
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "An error occurred while migrating the database or creating the admin user.");
+            throw;
+        }
+    });
+}
+//
 
 app.UseCors("AllowLocalhost");
 
